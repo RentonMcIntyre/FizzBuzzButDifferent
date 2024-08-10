51ba20a221e76376dc4895c098ec151a581c3304
Ren McIntyre <rentonmcintyre@yahoo.com>
2024-08-10 12:14:59 +0100

    Implemented actual console app using created Runner

--- FizzBuzzButDifferent/Program.cs
+++ FizzBuzzButDifferent/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using FizzBuzzRuleRunner;
+
+Runner runner = new();
+runner.LoadRules();
+
+var numberList = Enumerable.Range(1, 100).ToList();
+
+foreach (var number in numberList)
+{
+    var ruleResult = runner.GetRuleResult(number);
+    Console.WriteLine(ruleResult);
+}
